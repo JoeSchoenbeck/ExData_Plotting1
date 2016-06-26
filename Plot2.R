317f8e4313fa9ac0673a6217c5490b98b1622a2b
JoeSchoenbeck <Joe.Schoenbeck@gmail.com>
2016-06-26 14:37:15 -0500

    Delete Plot2.R

--- Plot2.R
+++
@@ -1,4 +0,0 @@
-#Plot 2
-par(mar = c(3,3,3,3))
-plot(data = HPCs, x = HPCs$datetime, y = HPCs$Global_active_power,type = "n",main = "Global Active Power",xlab = "", ylab = "Global Active Power (kilowatts)")
-lines(HPCs$datetime,HPCs$Global_active_power)
